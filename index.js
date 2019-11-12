551b374a111bd903ec84ebeadc8d2ee360db9b00
Chris Engel <Chris.Engel@yahoo.com>
2019-11-12 17:44:07 -0500

    8 down

--- index.js
+++ index.js
@@ -180,8 +180,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let runnerData =[];
+  runners.forEach(function(name){
+    return runnerData.push(`${name.last_name}, ${name.first_name}`)
+  });
+  return runnerData;
 }
 
 /**
